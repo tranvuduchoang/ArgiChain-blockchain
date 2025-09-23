475dffca8efec9b92d6aa9e5709a754a00201e96
unknown <tranvuduchoanghoang@gmail.com>
2025-09-23 08:55:23 +0700

    mint NFT success

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,20 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    // BSC Testnet
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC_URL || "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 97,
+      gasPrice: 10000000000, // 10 gwei
+    },
+    // BSC Mainnet
+    bsc: {
+      url: process.env.BSC_RPC_URL || "https://bsc-dataseed.binance.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 56,
+      gasPrice: 5000000000, // 5 gwei
+    },
     // Polygon Mumbai Testnet
     mumbai: {
       url: process.env.MUMBAI_RPC_URL || "https://rpc-mumbai.maticvigil.com",
@@ -44,6 +58,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      bscTestnet: process.env.BSCSCAN_API_KEY || "",
       polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
     },
   },
